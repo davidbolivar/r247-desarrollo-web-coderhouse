610d54148ca54a69d8e19162861ba472e4196016
davidbolivar <davidbolivarh@gmail.com>
2021-09-22 17:28:36 -0300

    Correción en eventListener de formulario de recarga

--- js/app.js
+++ js/app.js
@@ -7,12 +7,12 @@ let sendButton = formRecargar.elements["send-button"];
 formRecargar.addEventListener("submit", function (event) {
 	// stop form submission
 	event.preventDefault();
-	let name = prompt("¡Hola! ¿Cuál es tu nombre?");
+	let name = prompt("¡Hola! Por favor ingresa tu nombre para poder brindarte atención personalizada. Puedes dejar el campo en blanco si lo deseas.");
 
 	if (name && name != "") {
-		window.open(`https://wa.me/584120663036?text=¡Hola! Mi nombre es ${name.toUpperCase()}. Me interesa realizar una recarga *${operator.value.toUpperCase()}* por *${amount.value} Bs* al número *${serviceNumber.value}*.`, "_blank");
+		window.open(`https://wa.me/584120663036?text=*¡Hola!* Mi nombre es _*${name.toUpperCase()}*_. Me interesa realizar una recarga *${operator.value.toUpperCase()}* por *${amount.value} Bs* al número *${serviceNumber.value}*.`, "_blank");
 	} else if (name == "") {
-		window.open(`https://wa.me/584120663036?text=¡Hola! Me interesa realizar una recarga *${operator.value.toUpperCase()}* por *${amount.value} Bs* al número *${serviceNumber.value}*.`, "_blank");
+		window.open(`https://wa.me/584120663036?text=*¡Hola!* Me interesa realizar una recarga *${operator.value.toUpperCase()}* por *${amount.value} Bs* al número *${serviceNumber.value}*.`, "_blank");
 	} else {
 		alert("Intenta enviar tu solicitud nuevamente.");
 	}
