70fb46d9d4facec9a6921d7dfcdd4e47857bc352
davidbolivar <davidbolivarh@gmail.com>
2021-09-22 17:14:44 -0300

    Se agregó javascript al formulario de recarga

---
+++ js/app.js
@@ -0,0 +1,19 @@
+let formRecargar = document.getElementById("form-recargar");
+let operator = formRecargar.elements["operator"];
+let amount = formRecargar.elements["amount"];
+let serviceNumber = formRecargar.elements["service-number"];
+let sendButton = formRecargar.elements["send-button"];
+
+formRecargar.addEventListener("submit", function (event) {
+	// stop form submission
+	event.preventDefault();
+	let name = prompt("¡Hola! ¿Cuál es tu nombre?");
+
+	if (name && name != "") {
+		window.open(`https://wa.me/584120663036?text=¡Hola! Mi nombre es ${name.toUpperCase()}. Me interesa realizar una recarga *${operator.value}* por *${amount.value} Bs* al número *${serviceNumber.value}*`);
+	} else if (name == "") {
+		window.open(`https://wa.me/584120663036?text=¡Hola! Me interesa realizar una recarga *${operator.value.toUpperCase()}* por *${amount.value} Bs* al número *${serviceNumber.value}*`);
+	} else {
+		alert("Intenta enviar tu solicitud nuevamente.");
+	}
+});
