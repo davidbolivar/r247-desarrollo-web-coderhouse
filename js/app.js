08efc481d334ad8b4ddf22e8d6180211f4d6ec19
davidbolivar <davidbolivarh@gmail.com>
2021-09-23 15:56:24 -0300

    Corrección de logica para botones de operadoras

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Declaración de variables
+// Declaración de variables para envío de formulario por whatsapp
 let formRecargar = document.getElementById("form-recargar");
 let operator = formRecargar.elements["operator"];
 let amount = formRecargar.elements["amount"];
@@ -13,18 +13,24 @@ formRecargar.addEventListener("submit", function (event) {
 	window.open(`https://wa.me/584120663036?text=*¡Hola!* Me interesa realizar una recarga *${operator.value.toUpperCase()}* por *${amount.value} Bs* al número *${serviceNumber.value}*.`, "_blank");
 });
 
-const operators = ["movistar", "digitel", "movilnet", "movistar-tv", "simple-tv", "inter-prepago", "inter-postpago"];
+let operadoras = document.getElementById("operadoras");
+// Si existe el section de operadoras
+if (operadoras) {
+	// Array con lista de operadoras
+	const operators = ["movistar", "digitel", "movilnet", "movistar-tv", "simple-tv", "inter-prepago", "inter-postpago"];
 
-operators.forEach(function (ele, i) {
-	var element = document.querySelector("#" + ele + " button");
+	// Por cada operadora se crea un eventListener para llenar el formulario de recaga.
+	operators.forEach(function (ele, i) {
+		var element = document.querySelector("#" + ele + " button");
 
-	if (element) {
 		element.addEventListener("click", function (event) {
-			operator.value = ele;
+			// Al detectar el evento click, se limpia el formulario y se completa con la operadora seleccionada.
+			formRecargar.reset();
 			location.href = "#form-recargar";
 			setTimeout(function () {
+				operator.value = ele;
 				sendButton.click();
-			}, 700);
+			}, 1000);
 		});
-	}
-});
+	});
+}
